f78c7f7c035c2fc44bb9b298cc90673f782d616b
saikirananumalla <saikirananumalla@gmail.com>
2024-03-18 16:23:30 -0400

    class activity mongoose

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,13 @@
 let BookInstance = require('../models/bookinstance');
+const Book = require("../models/book");
+const Author = require("../models/author");
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
+exports.show_all_books_status = async (res) => {
+  BookInstance.find({'status': {$eq: 'Available'}}, 'title status')
+      .populate('book')
+      .exec()
+      .then(lbs => {
+        res.send(lbs.map(b => b.book.title + " " + b.status));
+      })
+      .catch(err => res.send("books not found"));
 }
